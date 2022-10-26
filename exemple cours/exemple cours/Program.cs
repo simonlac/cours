30e1854714bf0fe7f2e284cbd10f6bd98c863443
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:52:41 -0400

    Add files via upload

---
+++ exemple cours/exemple cours/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemple_cours
+{
+//défénition de la classe person
+
+    public class Person
+    {
+        //déclaration des attributs
+        private string nom;
+        private string prenom;
+
+        private string ville;
+
+        //déclaration des propriétés
+
+        public string Nom
+         { 
+          get { return nom; }
+          set { nom = value; }
+         
+        }
+      
+      //constructeur sans paramètre de la classe personne
+      public Person()
+        {
+         Console.WriteLine("création d'une nouvelle personne");
+        }
+
+        //constructeur avec 2 paramètre - surcharge le constructeur
+        public Person(string p_nom, string p_prenom)
+        {
+        Console.WriteLine("création d'une nouvelle personne qui se nomme" + " "+ p_prenom+ " " +p_nom);
+        }
+
+        //Constructeur avec 3 paramètre - surcharger le constructeur
+        public Person(string p_nom, string p_prenom,string p_ville)
+        {
+            this.nom = p_nom;
+            this.prenom = p_prenom;
+            this.ville = p_ville;
+            Console.WriteLine("création d'une nouvelle personne qui se nomme" + " " + p_prenom + " " + p_nom +
+                " et qui habite à " +p_ville );
+        }
+        //déstructeur de la classe person
+        ~Person()
+        {
+            Console.WriteLine("Destruction d'un objet de la classe person");
+            Console.ReadKey();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //déclaration de la référence
+            Person Simon;
+            // instanciation
+            Simon = new Person();
+
+            // déclaration et instanciation
+            Person Mathis = new Person();
+           
+
+
+           //déclaration et instanciation avec paramètre
+           Person Patrick = new Person("Bolduc","Patrick");
+          
+            //déclaration et instanciation avec paramètre
+            Person isabelle = new Person("dubois", "isabelle", "Val-des-Monts");
+            
+            //propriété nom
+            Console.WriteLine(isabelle.Nom);
+            // propriété nom, setter
+            isabelle.Nom = "Gauthier";
+            Console.WriteLine(isabelle.Nom);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exemple cours/exemple cours/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("exemple cours")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cégep de l'Outaouais")]
+[assembly: AssemblyProduct("exemple cours")]
+[assembly: AssemblyCopyright("Copyright © Cégep de l'Outaouais 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("a8ba18b9-28a2-4e58-a77f-b6f18f471c5b")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
